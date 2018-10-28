46a0a10f88e3d2ee3c723118c48f6426506fa8c0
pokotsun <gorigolila@gmail.com>
2018-10-28 12:15:04 +0900

    set data

--- tweet_img_getter/main.go
+++ tweet_img_getter/main.go
@@ -14,32 +14,36 @@ func main() {
 	config := config.GetConfig()
 	api := config.GetTwitterApi()
 	v := url.Values{}
-	v.Set("count", "450")
+	v.Set("count", "500")
 
-	result, err := api.GetSearch(config.HashTagNames[0], nil)
-	if err != nil {
-		panic(err)
-	}
-	tweets := result.Statuses
+	for _, hashtagName := range config.HashTagNames {
+		result, err := api.GetSearch(hashtagName, nil)
+		if err != nil {
+			panic(err)
+		}
+		tweets := result.Statuses
 
-	log.Println("TWEET_COUNTS: ", len(tweets))
-	for _, tweet := range tweets {
-		tweetInfo := new(models.TweetInfo)
-		var imageInfos models.ImageInfos
+		log.Println("TWEET_COUNTS: ", len(tweets))
+		for _, tweet := range tweets {
+			tweetInfo := new(models.TweetInfo)
+			var imageInfos models.ImageInfos
 
-		// json, _ := json.Marshal(tweet)
-		// log.Println("TWEET_DATA:  ", string(json))
-		for _, media := range tweet.ExtendedEntities.Media {
-			if media.Type == "photo" {
-				imageURL := media.Media_url
-				if !models.ImageInfoIsRetweeted(imageURL) {
-					imageInfos = append(imageInfos, models.ImageInfo{ImageURL: imageURL})
+			// json, _ := json.Marshal(tweet)
+			// log.Println("TWEET_DATA:  ", string(json))
+			log.Println("TWEET_TEXT: ", tweet.Text)
+			for _, media := range tweet.ExtendedEntities.Media {
+				if media.Type == "photo" {
+					imageURL := media.Media_url
+					if !models.ImageInfoIsRetweeted(imageURL) {
+						imageInfos = append(imageInfos, models.ImageInfo{ImageURL: imageURL})
+					}
 				}
 			}
-		}
-		if len(imageInfos) > 0 {
-			tweetInfo.Images = imageInfos
-			tweetInfo.Create()
+			if len(imageInfos) > 0 {
+				tweetInfo.Images = imageInfos
+				tweetInfo.Create()
+			}
 		}
 	}
+
 }
